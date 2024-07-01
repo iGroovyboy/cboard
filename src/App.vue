e8ecbc4339202841143760b93fc1fdcb77b04683
Tony Babintsev <tbabintsev2@gmail.com>
2024-07-01 17:14:58 +0300

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <template>
-  <nav>
-    <ul class="tabs flex flex-row cursor-pointer">
-      <li @contextmenu="contextMenu($event, Folder.Clipboard)" @click="switchTab(Folder.Clipboard)"
-        class="relative p-3 px-5 text-xs sm:text-base border-b flex shrink-0"
-        :class="{ active: activeTabId === Folder.Clipboard, 'border-gray-500': activeTabId !== Folder.Clipboard }">
-        <div class="z-1">Clipboard</div>
-        <div v-if="data[Folder.Clipboard].children.length"
-          class="opacity-80 z-0 chip absolute text-[10px] m-0 p-0 right-0 top-1 px-[2px] rounded-md">
-          {{ data[Folder.Clipboard].children.length }}
-        </div>
-      </li>
-      <li @contextmenu="contextMenu($event, Folder.Clipboard)" @click="switchTab(Folder.Favorites)"
-        class="relative p-3 px-5 text-xs sm:text-base border-b flex shrink-0"
-        :class="{ active: activeTabId === Folder.Favorites, 'border-gray-500': activeTabId !== Folder.Favorites }">
-        <img src="./assets/star.svg" alt="" class="w-4 mt-[-3px] mr-1">
-        <div class="z-1">Favorites</div>
-        <div v-if="data[Folder.Favorites].children.length"
-          class="opacity-80 z-0 chip absolute text-[10px] m-0 p-0 right-0 top-1 px-[2px] rounded-md">
-          {{ data[Folder.Favorites].children.length }}
-        </div>
-      </li>
-      <!-- <li class="p-3 px-5 text-xs sm:text-base border-b border-gray-500 flex shrink-0">
-        <img src="./assets/add.svg" alt="" class="mt-[-3px]">
-        Add
-      </li> -->
-      <li class="w-[-webkit-fill-available] p-3 px-5 border-b border-gray-500"></li>
-      <li @click="menuType = MENU_TYPE.Main" class="py-2 border-b border-gray-500">
-        <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="20px" preserveAspectRatio="xMidYMid meet">
-          <path fill="white" d="M3 18v-2h18v2Zm0-5v-2h18v2Zm0-5V6h18v2Z" />
-        </svg>
-      </li>
-    </ul>
-
-    <div class="search flex flex-row p-2">
-      <input type="text" placeholder="Search" class="p-1 text-xs sm:text-base w-11/12 border-sky-500">
-      <img src="./assets/search.svg" alt=""
-        class="w-8 max-h-10 sm:w-1/12 pl-2 opacity-30 hover:opacity-100 cursor-pointer">
-    </div>
-  </nav>
+  <app-tabs :active-tab-id="activeTabId" :clip-len="data[Folder.Clipboard].children.length"
+    :fav-len="data[Folder.Favorites].children.length" @switch-tab="switchTab" @mainmenu="menuType = MENU_TYPE.Main"
+    @contextmenu="contextMenu" />
 
   <main class="ml-2 mr-1 overflow-y-scroll overflow-x-hidden pr-1">
     <ul v-if="data && data[activeTabId] && data[activeTabId].children">
-      <li v-for="(item, key) in data[activeTabId].children" :key="key" class="flex pb-2 mb-2 border-b border-neutral-700">
+      <li v-for="( item, key ) in  data[activeTabId].children " :key="key"
+        class="flex pb-2 mb-2 border-b border-neutral-700">
         <div class="item w-11/12 cursor-pointer" @click="pasteItem(item)">
           <div class="value text-xs sm:text-base pb-2 mb-2 leading-5 overflow-hidden max-h-14">{{ item.contents }}</div>
           <div class="meta text-xs text-neutral-500">{{ formatDate(getTimestamp(item.name)) }}</div>
@@ -68,6 +33,7 @@ import { ClipboardData } from './common/interfaces';
 import { getFilesData } from './services/backend';
 import { formatDate } from './common/helpers';
 import AppPopup from './components/AppPopup.vue';
+import AppTabs from './components/AppTabs.vue';
 
 const invoke = window.__TAURI__.invoke;
 
@@ -145,7 +111,6 @@ onBeforeMount(async () => {
   await fetchData();
 })
 
-
 </script>
 
 <style lang="scss" scoped>
@@ -163,9 +128,5 @@ button {
   &:hover {
     opacity: 1;
   }
-}
-
-.active {
-  @apply text-sky-500 border-sky-500;
 }
 </style>
--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -1,23 +1,23 @@
 <template>
     <nav>
         <ul class="tabs flex flex-row cursor-pointer">
-            <li @contextmenu="contextMenu($event, Folder.Clipboard)" @click="switchTab(Folder.Clipboard)"
+            <li @contextmenu="$emit('contextmenu', $event, Folder.Clipboard)" @click="$emit('switch-tab', Folder.Clipboard)"
                 class="relative p-3 px-5 text-xs sm:text-base border-b flex shrink-0"
                 :class="{ active: activeTabId === Folder.Clipboard, 'border-gray-500': activeTabId !== Folder.Clipboard }">
                 <div class="z-1">Clipboard</div>
-                <div v-if="data[Folder.Clipboard].children.length"
+                <div v-if="clipLen"
                     class="opacity-80 z-0 chip absolute text-[10px] m-0 p-0 right-0 top-1 px-[2px] rounded-md">
-                    {{ data[Folder.Clipboard].children.length }}
+                    {{ clipLen }}
                 </div>
             </li>
-            <li @contextmenu="contextMenu($event, Folder.Clipboard)" @click="switchTab(Folder.Favorites)"
+            <li @contextmenu="$emit('contextmenu', $event, Folder.Clipboard)" @click="$emit('switch-tab', Folder.Favorites)"
                 class="relative p-3 px-5 text-xs sm:text-base border-b flex shrink-0"
                 :class="{ active: activeTabId === Folder.Favorites, 'border-gray-500': activeTabId !== Folder.Favorites }">
                 <img src="./assets/star.svg" alt="" class="w-4 mt-[-3px] mr-1">
                 <div class="z-1">Favorites</div>
-                <div v-if="data[Folder.Favorites].children.length"
+                <div v-if="favLen"
                     class="opacity-80 z-0 chip absolute text-[10px] m-0 p-0 right-0 top-1 px-[2px] rounded-md">
-                    {{ data[Folder.Favorites].children.length }}
+                    {{ favLen }}
                 </div>
             </li>
             <!-- <li class="p-3 px-5 text-xs sm:text-base border-b border-gray-500 flex shrink-0">
@@ -25,7 +25,7 @@
         Add
       </li> -->
             <li class="w-[-webkit-fill-available] p-3 px-5 border-b border-gray-500"></li>
-            <li @click="mainMenuShown = !mainMenuShown" class="py-2 border-b border-gray-500">
+            <li @click="$emit('mainmenu')" class="py-2 border-b border-gray-500">
                 <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="20px" preserveAspectRatio="xMidYMid meet">
                     <path fill="white" d="M3 18v-2h18v2Zm0-5v-2h18v2Zm0-5V6h18v2Z" />
                 </svg>
@@ -41,4 +41,26 @@
 </template>
 
 <script setup lang="ts">
+import { Folder } from '../common/constants';
+
+defineProps({
+    activeTabId: {
+        type: Number,
+        default: Folder.Clipboard,
+    },
+    clipLen: {
+        type: Number,
+        default: 0,
+    },
+    favLen: {
+        type: Number,
+        default: 0,
+    },
+});
 </script>
+
+<style lang="scss" scoped>
+.active {
+    @apply text-sky-500 border-sky-500;
+}
+</style>
